7929123c00a47415ac6405030029385cf5c941e0
Rafaela Mayumi Wada Fukuda <125597771+RafaelaMayumiFukuda@users.noreply.github.com>
2023-10-16 16:57:19 -0300

    Add files via upload

---
+++ JS.sql
@@ -0,0 +1,11 @@
+create database aviao;
+use aviao
+
+create table aviao(
+id int auto_increment not null,
+nomeaviao varchar(50) null,
+tipo varchar(50) null,
+primary key(id)
+);
+
+select * from aviao;
